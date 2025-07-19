5cee4c8db2f64accc551b09f62423de2ae193d55
Aditya_Magdum <magdumadiraj@gmail.com>
2025-07-19 12:27:18 +0530

    Project setup

---
+++ src/main/java/com/project/personalfinancemanager/PersonalfinancemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.personalfinancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalfinancemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalfinancemanagerApplication.class, args);
+	}
+
+}
